c6daeb6e1dc01b7896e0b47f4e0d40f7b49cc978
Deepin <deepin.pradhananga@gurzu.net>
2022-02-03 12:47:44 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Portfolio - Deepin Pradhananga
+
+This project is a simple portofolio website as an assigment task for bootcamp by Gurzu.
+
+Project is created using:
+1. Html 
+2. CSS
+3. Java Script
+
+Project folders
+- Portfolio
+	- font
+	- images
---
+++ project.js
@@ -0,0 +1,20 @@
+const navSlide = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelector('.nav-links li');
+
+    hamburger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, _index) => {
+            if (link.style.animation) {
+                link.style.animation ='';
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s';
+            }
+        });
+    });
+
+   
+}
+navSlide();
